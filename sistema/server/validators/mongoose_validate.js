68b1c8476aa8b74e73f70a945f56584b3be4e611
Suissa <jnascimento@gmail.com>
2015-09-30 02:57:34 -0300

    Validador do save mongoose - ideia

--- sistema/server/validators/mongoose_validate.js
+++ sistema/server/validators/mongoose_validate.js
@@ -15,3 +15,28 @@ const Returns = [
 function testReturn(action, data) {
   console.log('action', action, data)
 };
+
+// save
+// Deve retornar o mesmo objeto do Schema acrescido de:
+// _id
+// __v
+// Verificar cada valor persistido
+
+// var saveReturn = { __v: 0,
+//   _id: "",
+//   created_at: "",
+//   category: '""',
+//   price: "",
+//   alcohol: "",
+//   description: "",
+//   name: "" }
+
+// save
+// { __v: 0,
+//   _id: 560b790c5747b5491c662170,
+//   created_at: Wed Sep 30 2015 02:54:20 GMT-0300 (BRT),
+//   category: 'pilsen',
+//   price: 3,
+//   alcohol: 4.5,
+//   description: 'Mijo de rato',
+//   name: 'Skol' }
