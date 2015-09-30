d3d27542afe7b5d616c38bb301f258d54b8a4327
Pompeu <itacir@hotmail.com>
2015-09-30 01:46:14 -0300

    mongoose connect here will be remove on app.js done

--- sistema/modelagem/user/user_model.js
+++ sistema/modelagem/user/user_model.js
@@ -1,18 +1,22 @@
+'use strict';
+
 const MODEL_NAME = 'User';
 const mongoose = require('mongoose');
-const skeleton = require('./user_schema');
-const Schema = new mongoose.Schema(skeleton);
-const Model = mongoose.model(MODEL_NAME, Schema);
+mongoose.connect('localhost', 'instagram');
+const skeconston = require('./user_schema');
+const Schema = mongoose.Schema;
+const schema = new Schema(skeconston);
+const Model = mongoose.model(MODEL_NAME, schema);
 
 // const ValidateEmail = require('./email_validate');
 // const ValidatePassword = require('./password_validate');
 
 // Model.schema.path('email').validate(ValidateEmail.validator, ValidateEmail.message);
 
-var _model = new Model();
+//var _model = new Model();
 
-_model.email = 'tesssssst';
-_model.senha = 'SENHAA';
-console.log(_model.validateSync().toString());
+//_model.email = 'tesssssst';
+//_model.senha = 'SENHAA';
+//console.log(_model.validateSync());
 
 module.exports = Model;
