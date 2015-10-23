be72694fbcbe21e38c885c7c8618d00df9da7f9c
Pompeu <itacir@hotmail.com>
2015-10-23 15:43:22 -0200

    new test for images upload in create added

--- sistema-mongoose-atomico-promises-events/server/tests/photos-routes.test.js
+++ sistema-mongoose-atomico-promises-events/server/tests/photos-routes.test.js
@@ -7,23 +7,28 @@ let url = require('url');
 const ABS_URL = "http://localhost:3000/api/photos";
 
 describe('photos routes testing', () => {
-	let id = null
-		, photo = {	
-				legend : ch.Lorem.words({wordCount : 5, supplemental : false})
-		}
-		;
-
+	let id = null;
+	let photo = {
+		legend : ch.Lorem.word()
+	};
+	
 	it('it should be an user create one photo' , done => {
 		request
-			.post(url.resolve(ABS_URL, 'photos'))
+			.post(url.resolve(ABS_URL, './photos'))
 			.send(photo)
+			.attach('image', '../server/tests/test.png')
+			.field('legend', photo.legend)
 			.end((err, res) => {
 				res.should.be.exist;
 				res.body._id.should.exist;
+				res.body.legend.should.be.eql(photo.legend);
+				res.body.path.should.exist;
 				id = res.body_id;
 				res.status.should.be.eql(201);
 				done();
 			});
+
 	});
+	
 });
 
