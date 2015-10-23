6bd3c670d4f2796fbf2984050d0d44bcdb38959d
Pompeu <itacir@hotmail.com>
2015-10-23 04:22:16 -0200

    debug in use now for this route

--- sistema-mongoose-atomico-promises-events/server/modules/photos/actions/action.create.js
+++ sistema-mongoose-atomico-promises-events/server/modules/photos/actions/action.create.js
@@ -4,15 +4,17 @@ let photo = require('../photo_model');
 
 const Action = {
 		create: function(req, res) {
-		console.log('create photo')
+		debug('Action Create');
 		let promise = photo.create(req.body);
 		promise.then(succesHandler , errorHandler)
 
 		function succesHandler (photo){
+			debug('Action Create secces');
 			res.status(201).json(photo);
 		}
 
 		function errorHandler (err) {
+			debug('Action Create error');
 			res.status(400).json(err);
 		}
   }
