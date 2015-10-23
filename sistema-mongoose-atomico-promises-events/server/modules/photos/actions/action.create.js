81e3b5727e3869cc5312e09273aafd5802c81078
Pompeu <itacir@hotmail.com>
2015-10-23 15:44:27 -0200

    action create now use formidable for parse image upload

--- sistema-mongoose-atomico-promises-events/server/modules/photos/actions/action.create.js
+++ sistema-mongoose-atomico-promises-events/server/modules/photos/actions/action.create.js
@@ -1,22 +1,40 @@
 'use strict';
 
 let photo = require('../photo_model');
+const formidable = require('formidable');
 
 const Action = {
 		create: function(req, res) {
 		debug('Action Create');
-		let promise = photo.create(req.body);
-		promise.then(succesHandler , errorHandler)
 
-		function succesHandler (photo){
-			debug('Action Create secces');
-			res.status(201).json(photo);
-		}
+		formParse(req, (err, result) => {
+			let promise = photo.create(result);
+			promise.then(succesHandler , errorHandler)
 
-		function errorHandler (err) {
-			debug('Action Create error');
-			res.status(400).json(err);
-		}
+			function succesHandler (photo){
+				debug('Action Create secces');
+				res.status(201).json(photo);
+			}
+
+			function errorHandler (err) {
+				debug('Action Create error');
+				res.status(400).json(err);
+			}	
+		});
   }
 }
+
+function formParse(req , cb) {
+	let form = new formidable.IncomingForm();
+	form.uploadDir = "../server/public/image";
+	form.parse(req, (err, fields, files) => {
+		if(!err) { 
+			return cb(null, {
+					path : files.image.path
+				,	legend : fields.legend
+				});
+		}
+		return cb(err);
+	});
+} 
 module.exports = Action;
