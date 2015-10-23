7ff3557f510e381bf24cbcf8b3d3d0f575687ebe
Pompeu <itacir@hotmail.com>
2015-10-23 15:47:37 -0200

    same refactory in identation

--- sistema-mongoose-atomico-promises-events/server/modules/photos/photo_schema.js
+++ sistema-mongoose-atomico-promises-events/server/modules/photos/photo_schema.js
@@ -3,26 +3,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const _schema = {
-  user_id: {
-    type: Schema.ObjectId
-  , ref: 'User'
+		user_id: {
+			type: Schema.ObjectId
+		, ref: 'User'
 	}
-, legend: {
-    type: String
-  , default: ''
-  }
-, path: {type: String, default: '' }
-, likes: [
-    {
-      user_id: { type : Schema.ObjectId , ref: 'User'	}
-    }
-  ]
-, comments: [
-    { user_id: { type: Schema.ObjectId , ref: 'User'	}
-    , comment: {  type: String , default: '' }
+	,	legend: {
+			type: String
+		, default: ''
 		}
-  ]
-, created_at: { type: Date, default: Date.now }
+	,	path: {type: String, default: '' }
+	,	likes: [
+			{
+				user_id: { type : Schema.ObjectId , ref: 'User'}
+			}
+		]
+	,	comments: [
+			{ 
+				user_id: { type: Schema.ObjectId , ref: 'User'}
+			,	comment: {  type: String , default: '' }
+			}
+		]
+	,	created_at:	{ type: Date, default: Date.now }
 }
 
 module.exports = _schema;
--- sistema-mongoose-atomico-promises-events/server/modules/photos/routes.js
+++ sistema-mongoose-atomico-promises-events/server/modules/photos/routes.js
@@ -2,15 +2,16 @@
 
 const ACTIONS_FOLDER = './actions/'
 		,	ROUTES_FOLDER = './routes/'
-		, ACTIONS = ['create', 'retrieve', 'update', 'delete']
+		,	ACTIONS = ['create', 'retrieve', 'update', 'delete']
 		;
+
 let Routes = [];
 
 ACTIONS.forEach( action => {
-  let Action = require(ACTIONS_FOLDER + 'action.'+ action +'.js');
-  let Config = require(ROUTES_FOLDER + 'route.'+ action +'.config');
-  let Route = require(ROUTES_FOLDER + 'route.default.js')(Action, Config);
-  Routes.push(Route);
+	let Action = require(ACTIONS_FOLDER + 'action.'+ action +'.js');
+	let Config = require(ROUTES_FOLDER + 'route.'+ action +'.config');
+	let Route = require(ROUTES_FOLDER + 'route.default.js')(Action, Config);
+	Routes.push(Route);
 });
 
 module.exports = Routes;
