577ec24d8ef197428db1316687d0d08638ab9009
Pompeu <itacir@hotmail.com>
2015-11-05 16:58:17 -0200

    delete get all done update need reapair

--- sistema-mongoose-atomico-promises-events/server/tests/photos-routes.test.js
+++ sistema-mongoose-atomico-promises-events/server/tests/photos-routes.test.js
@@ -5,17 +5,32 @@ const should = require('chai').should()
   , request =  require('superagent')
   , url = require('url')
   , ABS_URL = "http://localhost:3000/api/photos"
+  , ABS_URL_USERS = "http://localhost:3000/api/users"
   , fs =  require('fs')
+  , dir = __dirname.replace('tests', 'public/images/')
   ;
 
+
 describe('photos routes testing', () => {
-  let id = nulli;
-  const photo = {
-    legend : ch.Lorem.word()
-  };
+  let id = null
+    ,  user_id = null
+    , photo = {
+          user_id : ''
+        , legend : ch.Lorem.word()
+      }
+    ;
+
+  before('get id for one user',done => {
+   request
+      .get(url.resolve(ABS_URL_USERS,'users'))
+      .end((err, res) => {
+          user_id = photo.user_id = res.body[0]._id;
+        done();
+      });
+  });
 
   after('after all', done => {
-    let path    = '../server/public/image/'
+    let path    = dir 
       , images  = fs.readdirSync(path)
       ;
     images.forEach(img => {
@@ -24,21 +39,54 @@ describe('photos routes testing', () => {
     done();
   });
 
-  it('it should be an user create one photo' , done => {
+  it('should be an user create one photo' , done => {
     request
-      .post(url.resolve(ABS_URL, './photos'))
+      .post(url.resolve(ABS_URL, 'photos'))
       .send(photo)
-      .attach('image', '../server/tests/test.png')
+      .attach('image', __dirname+'/test.png')
       .field('legend', photo.legend)
+      .field('user_id', photo.user_id)
       .end((err, res) => {
         res.should.be.exist;
         res.body._id.should.exist;
         res.body.legend.should.be.eql(photo.legend);
+        res.body.user_id.should.be.eql(photo.user_id);
         res.body.path.should.be.not.empty;
-        id = res.body_id;
+        id = res.body._id;
         res.status.should.be.eql(201);
         done();
       });
   });
-  
+
+  it('should be get all photos', done => {
+    request
+        .get(url.resolve(ABS_URL, 'photos'))
+        .end((err, res) => {
+          res.body.should.be.an('Array');
+          res.status.should.be.eql(200);
+          done();
+        });
+  });
+
+  it('should be update one phote by id', done => {
+    let newPhoto = {
+          user_id : user_id
+        , legend : ch.Lorem.word()
+    }
+    request
+      .put(url.resolve(ABS_URL, 'photos/'+id))
+      .send(newPhoto)
+      .end((err, res) => {
+       done();
+      })
+  });
+
+  it('should be delete one photo by id', done => {
+    request
+      .del(url.resolve(ABS_URL, 'photos/'+id))
+      .end((err, res) => {
+        res.status.should.be.eql(204)
+        done();
+      });
+  });
 });
