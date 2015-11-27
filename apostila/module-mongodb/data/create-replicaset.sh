ead95173dd4dfb61305c8a2ee8a8becf01c864e5
Suissa <jnascimento@gmail.com>
2015-11-26 23:10:26 -0200

    Replica set

---
+++ apostila/module-mongodb/data/create-replicaset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mongod --replSet replica_set --port 27117 --dbpath /data/rs1 --logpath /data/rs1/log.txt --fork
+mongod --replSet replica_set --port 27118 --dbpath /data/rs2 --logpath /data/rs1/log.txt --fork
+mongod --replSet replica_set --port 27119 --dbpath /data/rs3 --logpath /data/rs1/log.txt --fork
