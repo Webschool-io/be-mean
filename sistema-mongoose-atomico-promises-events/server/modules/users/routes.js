e31a769abb47aa812dbd41d8cd3aa2ab4b1ffa05
Pompeu <itacir@hotmail.com>
2015-12-06 03:15:19 -0200

    remove some conflict

--- sistema-mongoose-atomico-promises-events/server/modules/users/routes.js
+++ sistema-mongoose-atomico-promises-events/server/modules/users/routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 const ACTIONS_FOLDER = './actions/';
@@ -12,20 +11,3 @@ ACTIONS.forEach(function(action) {
   const Route = require(ROUTES_FOLDER + 'route.default.js')(Action, Config);
   Routes.push(Route);
 });
-
-module.exports = Routes;
-=======
-const ACTIONS_FOLDER = './actions/';
-const ROUTES_FOLDER = './routes/';
-const ACTIONS = ['create', 'retrieve', 'update', 'delete'];
-var Routes = [];
-
-ACTIONS.forEach(function(action) {
-  const Action = require(ACTIONS_FOLDER + 'action.'+ action +'.js');
-  const Config = require(ROUTES_FOLDER + 'route.'+ action +'.config');
-  const Route = require(ROUTES_FOLDER + 'route.default.js')(Action, Config);
-  Routes.push(Route);
-});
-
-module.exports = Routes;
->>>>>>> feb4b8bc3ac09a0c2fb371dbb1aecd8fcec3a5ca
