5a1f17653f95a0477d6cde746be9212b80cfdb32
Pompeu <itacir@hotmail.com>
2015-12-06 03:18:19 -0200

    remove some conflict last time

--- sistema-mongoose-atomico-promises-events/server/modules/users/fields/config.fields.js
+++ sistema-mongoose-atomico-promises-events/server/modules/users/fields/config.fields.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 const TYPES_PATH = './../types/';
@@ -10,14 +9,3 @@ const Config = {
 };
 
 module.exports = Config;
-=======
-const TYPES_PATH = './../types/';
-const VALIDATORS_PATH = './../validators/';
-
-const Config = {
-  TYPES_PATH: TYPES_PATH
-, VALIDATORS_PATH: VALIDATORS_PATH
-}
-
-module.exports = Config;
->>>>>>> feb4b8bc3ac09a0c2fb371dbb1aecd8fcec3a5ca
