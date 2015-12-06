9e3e7d8c840e5716f1eb1aae65bafa1c355fc9e5
Pompeu <itacir@hotmail.com>
2015-12-06 03:16:46 -0200

    remove some conflict second time

--- sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.create.js
+++ sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.create.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 let user = require('../user_model');
@@ -20,25 +19,3 @@ const Action = {
 };
 
 module.exports = Action;
-=======
-'use strict';
-
-let user = require('../user_model');
-
-const Action = {
-		create: function(req, res) {
-
-		let promise = user.create(req.body);
-		promise.then(succesHandler , errorHandler)
-
-		function succesHandler (user){
-			res.status(201).json(user);
-		}
-
-		function errorHandler (err) {
-			res.status(400).json(err);
-		}
-  }
-}
-module.exports = Action;
->>>>>>> feb4b8bc3ac09a0c2fb371dbb1aecd8fcec3a5ca
--- sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.delete.js
+++ sistema-mongoose-atomico-promises-events/server/modules/users/actions/action.delete.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 let user = require('../user_model');
@@ -19,25 +18,3 @@ const Action = {
   }
 };
 module.exports = Action;
-=======
-'use strict';
-
-let user = require('../user_model');
-
-const Action = {
-  delete: function(req, res) {
-		let promise = user.remove({_id : req.params.id});
-		promise.then(succesHandler , errorHandler)
-
-		function succesHandler (user){
-			res.status(204).json(user);
-		}
-
-		function errorHandler (err) {
-			res.status(400).json(err);
-		}
-
-  }
-}
-module.exports = Action;
->>>>>>> feb4b8bc3ac09a0c2fb371dbb1aecd8fcec3a5ca
