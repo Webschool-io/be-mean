9bd195c7da392992ed93b1e8de25ba54fd9cff6d
Suissa <jnascimento@gmail.com>
2015-12-12 13:31:56 -0200

    Aula 3

--- Apostila/module-nodejs/src/http/http-get-localhost-querystring.js
+++ Apostila/module-nodejs/src/http/http-get-localhost-querystring.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 http.get({
   hostname: 'localhost',
-  path: '/teste?irru=true&xulepa=1',
+  path: '/teste?name=Suissa&teacher=1&age=31',
   port: 3000,
   agent: false
 }, (response) =>  {
