b1cdbbfe6105e08644ca74969377a6cb3fee109a
William Bruno <rocha_bruno@hotmail.com>
2016-02-07 17:05:46 -0200

    module-mongodb as git submodule

--- Apostila/module-mongodb/src/data/create-replicaset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mongod --replSet replica_set --port 27017 --dbpath /data/rs1 --logpath /data/rs1/log.txt --fork
-mongod --replSet replica_set --port 27018 --dbpath /data/rs2 --logpath /data/rs2/log.txt --fork
-mongod --replSet replica_set --port 27019 --dbpath /data/rs3 --logpath /data/rs3/log.txt --fork
