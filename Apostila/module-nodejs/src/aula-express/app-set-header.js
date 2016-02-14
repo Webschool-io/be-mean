13430f7d1be71182674eb3fa2f90f38840d043fc
pompeu <itacir@hotmail.com>
2016-02-14 09:13:38 -0200

    hellowor exemplo and set header done

--- Apostila/module-nodejs/src/aula-express/app-set-header.js
+++ Apostila/module-nodejs/src/aula-express/app-set-header.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get('/',function (req, res) {
-  res.send('Hello World!');
+  res.set({ 'Auth' :  '123sf'});
+  res.send({ hello :  'World!'});
 });
 
 app.listen(3000, function () {
